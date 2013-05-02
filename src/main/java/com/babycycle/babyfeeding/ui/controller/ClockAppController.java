56d021c357715a677b4ea35bb32e306ce17e05d0
Dmitry Kolesnikov <don.dimas@gmail.com>
2013-05-02 11:39:24 +0200

    AlarmClock open functionality improvement

--- src/main/java/com/babycycle/babyfeeding/ui/controller/ClockAppController.java
+++ src/main/java/com/babycycle/babyfeeding/ui/controller/ClockAppController.java
@@ -21,6 +21,81 @@ import com.babycycle.babyfeeding.R;
 public class ClockAppController {
 
     private Context context;
+    boolean foundClockImpl = false;
+
+    private Intent alarmClockIntent;
+
+    private void checkIfClockAppIsAccessible() {
+        PackageManager packageManager = context.getPackageManager();
+        alarmClockIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
+
+        // Verify clock implementation
+        String clockImpls[][] = {
+                {"HTC Alarm Clock", "com.htc.android.worldclock", "com.htc.android.worldclock.WorldClockTabControl" },
+                {"Standar Alarm Clock", "com.android.deskclock", "com.android.deskclock.AlarmClock"},
+                {"Froyo Nexus Alarm Clock", "com.google.android.deskclock", "com.android.deskclock.DeskClock"},
+                {"Moto Blur Alarm Clock", "com.motorola.blur.alarmclock",  "com.motorola.blur.alarmclock.AlarmClock"},
+                {"Samsung Galaxy Clock", "com.sec.android.app.clockpackage","com.sec.android.app.clockpackage.ClockPackage"}
+        };
+
+        foundClockImpl = false;
+
+        for(int i=0; i<clockImpls.length; i++) {
+            String vendor = clockImpls[i][0];
+            String packageName = clockImpls[i][1];
+            String className = clockImpls[i][2];
+            try {
+                ComponentName cn = new ComponentName(packageName, className);
+                ActivityInfo aInfo = packageManager.getActivityInfo(cn, PackageManager.GET_META_DATA);
+                alarmClockIntent.setComponent(cn);
+                Log.e("ClockAppController", "Found " + vendor + " --> " + packageName + "/" + className);
+                foundClockImpl = true;
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("ClockAppController", vendor + " does not exists");
+            }
+        }
+
+    }
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void openClockAppIfNeed() {
+        checkIfClockAppIsAccessible();
+        if (!foundClockImpl) {
+            return;
+        }
+        openConfirmationDialogue();
+    }
+
+    private void openConfirmationDialogue() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setPositiveButton(R.string.alarm_clock_open_confirm, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                openClockAppIntent();
+            }
+        });
+        builder.setNegativeButton(R.string.alarm_clock_open_cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setTitle(R.string.alarm_clock_open_title);
+        builder.create().show();
+    }
+
+    private void openClockAppIntent() {
+        if (foundClockImpl) {
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, alarmClockIntent, 0);
+            try {
+                pendingIntent.send();
+            } catch (PendingIntent.CanceledException e) {
+
+            }
+        }
+    }
+
 
     public void openClockAppIfPossible() {
         PackageManager packageManager = context.getPackageManager();
@@ -35,7 +110,7 @@ public class ClockAppController {
                 {"Samsung Galaxy Clock", "com.sec.android.app.clockpackage","com.sec.android.app.clockpackage.ClockPackage"}
         };
 
-        boolean foundClockImpl = false;
+        foundClockImpl = false;
 
         for(int i=0; i<clockImpls.length; i++) {
             String vendor = clockImpls[i][0];
@@ -60,26 +135,5 @@ public class ClockAppController {
 
             }
         }
-    }
-
-    public void setContext(Context context) {
-        this.context = context;
-    }
-
-    public void openClockAppIfNeed() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-
-        builder.setPositiveButton("Yes I DO!!!", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                openClockAppIfPossible();
-            }
-        });
-        builder.setNegativeButton("No thanks", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-
-            }
-        });
-        builder.setTitle(" Do you want to set Alarm Clock?");
-        builder.create().show();
     }
 }
